c7796c697daae0181a9edfee7ebb00f5267be777
8ima8 <fa.beltransalas@gmail.com>
2025-03-26 16:36:43 -0400

    Create UserCard component with different types of variables as PROPS

--- react-course/src/App.jsx
+++ react-course/src/App.jsx
@@ -3,7 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
-import { Post } from './components/post'
+import { Post } from './components/Post'
+import { UserCard } from './components/UserCard'
+
 
 function App() {
 
@@ -11,9 +13,20 @@ function App() {
   return (
     <>
       <p>Hola Mundo</p>
-      <Post/>
-      <Post/>
-      
+      <Post postNumber={1} />
+      <Post postNumber={2}/>
+      <UserCard 
+        name='Felipe'
+        amount={2000} 
+        married={false} 
+        array ={[22,44,6.6]} 
+        address={{street: 'Los Pinos', houseNumber:120, city:'La Paz'}}  />
+        <UserCard 
+        name='Maria'
+        amount={100000} 
+        married={true} 
+        array ={[90,95.5,80]} 
+        address={{street: 'Obrajes', houseNumber:200, city:'Santa Cruz'}}  />
     </>
   )
 }
