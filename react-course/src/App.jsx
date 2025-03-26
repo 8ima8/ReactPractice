0155c853d558db652ccc83d587e883baf233a48e
8ima8 <fabianbeltra@MacBook-Pro-de-Fabian.local>
2025-03-26 15:41:48 -0400

    PruebaComponentes mandando textos, numeros, array, object and function

---
+++ react-course/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import { Post } from './components/post'
+
+function App() {
+
+
+  return (
+    <>
+      <p>Hola Mundo</p>
+      <Post/>
+      <Post/>
+      
+    </>
+  )
+}
+
+export default App
